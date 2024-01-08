7b9bd381bf9e2dec14546d4448245687aeb78072
Élida Souza <elidacpsouza@gmail.com>
2024-01-08 11:46:38 -0300

    Initial jest tests

---
+++ src/app/components/Main/test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    // renderiza o component
+    render(<Main />)
+    // busca o elemento e verifica a existencia
+    expect(screen.getByRole('heading', { name: /react avançado/i })).toBeInTheDocument()
+  })
+})
