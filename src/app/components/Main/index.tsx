980db941d065d11e4d8714f2a9d4bded4f6fb1b3
Élida Souza <elidacpsouza@gmail.com>
2024-01-09 10:34:06 -0300

    add style config

--- src/app/components/Main/index.tsx
+++ src/app/components/Main/index.tsx
@@ -1,8 +1,9 @@
+import * as S from './styles'
 const Main = () => {
   return (
-    <main>
+    <S.Wrapper>
       <h1>React Avançado</h1>
-    </main>
+    </S.Wrapper>
   )
 }
 export default Main
